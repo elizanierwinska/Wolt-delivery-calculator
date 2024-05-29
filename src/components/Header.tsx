281ae602190631e9c32b0032e25a7f1ee0c5fae4
Dromuarga <110127268+Dromuarga@users.noreply.github.com>
2024-05-29 17:29:11 +0200

    Change background image to color and adjust the main container

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import '../index.css';
 export default function Header() {
   return (
     <header className="App-header">
-      <img id="logo" src="wolt-logo.png" alt="wolt company logo" />
+      <img id="logo" src="https://em-content.zobj.net/source/emojipedia/328/light-blue-heart_.png" alt="wolt company logo" />
       <h2 id="title">Delivery fee calculator</h2>
     </header>
   );
